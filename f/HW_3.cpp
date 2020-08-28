87c49636accb21b159ee01b22865c1c35eab85f9
Napong Keha <gorngorn9876@hotmail.com>
2020-08-29 01:36:12 +0700

    v2

--- f/HW_3.cpp
+++ f/HW_3.cpp
@@ -3,6 +3,10 @@ int main() {
 	int row, i, j;
 	printf("Enter the number of layers : ");
 	scanf_s("%d", &row);
+	if (row < 0)
+	{
+		printf("ERROR");
+	}else
 	for (i = 1;row >= i;i++)
 	{
 		for (j = 1;i >= j;j++)
